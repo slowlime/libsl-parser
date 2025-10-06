bb6998c63e35104e6b591b197fc5bd6665965194
Alexander Yanin <472709@edu.itmo.ru>
2025-10-06 16:21:54 +0300

    Set up a test CLI

--- libsl2-compat/build.gradle.kts
+++ libsl2-compat/build.gradle.kts
@@ -9,11 +9,52 @@ repositories {
     mavenCentral()
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
+sourceSets {
+    create("cli") {
+        compileClasspath += sourceSets.main.get().output
+        runtimeClasspath += sourceSets.main.get().output
+    }
+}
+
+val cliImplementation by configurations.getting {
+    extendsFrom(configurations.implementation.get())
+}
+
+val cliRuntimeClasspath by configurations.getting
+
 dependencies {
     implementation(project(":libsl1"))
     implementation(project(":libsl2"))
+
+    cliImplementation("com.github.ajalt.clikt:clikt:5.0.3")
 }
 
-kotlin {
-    jvmToolchain(17)
+val cliUberJar = tasks.register<Jar>("cliUberJar") {
+    archiveBaseName = "libsl2-compat-cli"
+    archiveClassifier = "uber"
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from(sourceSets["cli"].output)
+    from(sourceSets.main.get().output)
+
+    dependsOn(cliRuntimeClasspath)
+
+    from(
+        cliRuntimeClasspath
+            .filter { it.name.endsWith(".jar") }
+            .map { zipTree(it) }
+    )
+
+    manifest {
+        attributes["Main-Class"] = "${project.group}.libsl2.compat.cli.MainKt"
+    }
+}
+
+tasks.named("assemble") {
+    dependsOn(cliUberJar)
 }
---
+++ libsl2-compat/src/cli/kotlin/org/jetbrains/research/libsl2/compat/cli/Main.kt
@@ -0,0 +1,48 @@
+package org.jetbrains.research.libsl2.compat.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.Context
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.help
+import com.github.ajalt.clikt.parameters.options.defaultLazy
+import com.github.ajalt.clikt.parameters.options.help
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.path
+import org.jetbrains.research.libsl2.compat.LibSLCompat
+import java.nio.file.Path
+
+private class LibSL2CompatCli : CliktCommand(name = "libsl2-compat-cli") {
+    override fun run() = Unit
+}
+
+private class Dump : CliktCommand() {
+    override fun help(context: Context): String = """
+        Parse a LibSL file and dump it back as text.
+    """.trimIndent()
+
+    private val basePath by option("-b", "--base-path")
+        .path(canBeFile = false)
+        .defaultLazy { Path.of(".") }
+        .help("The base path for resolving imports")
+
+    private val file by argument().file()
+        .help("The path to the LibSL file")
+
+    override fun run() {
+        val compat = LibSLCompat(basePath.toString())
+        val lib = compat.loadFromFile(file)
+
+        for (error in compat.libsl.errorManager.errors) {
+            echo(error.text, err = true)
+        }
+
+        echo(lib.dumpToString())
+    }
+}
+
+fun main(args: Array<String>) = LibSL2CompatCli()
+    .subcommands(Dump())
+    .main(args)
